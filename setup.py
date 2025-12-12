b6517401154ec936c255ef41220400d471fefdd8
Joe Marchionna <jmarchionna@foghorntx.com>
2025-12-12 12:40:57 -0500

    first commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools, json
 from setuptools import setup
 
 # This file should not be edited when changing versions, edit the package metadata file
-_metadata = json.load(open("msad-query/_metadata.json", "r"))
+_metadata = json.load(open("msad_query/_metadata.json", "r"))
 
 setup(
     name=_metadata["name"],
@@ -10,7 +10,7 @@ setup(
     author=_metadata["author"],
     author_email=_metadata["author_email"],
     description=_metadata["description"],
-    long_description=open("readme.md").read(),
+    long_description=open("readme.asciidoc").read(),
     license=open("license.md").read(),
     package_data=_metadata["packageData"],
     include_package_data=True,
